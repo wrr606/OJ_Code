951c8f7772fb61f3a385ff5365b97bca4d7b28fd
wrr606 <wrr606@gmail.com>
2023-03-20 11:31:49 +0800

    re

--- Zerojudge/C++.cpp
+++ Zerojudge/C++.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 #define endl '\n'
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end() 
+#define ll long long
 
 int main(){
     //ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
