4ca6b3dcdb611f4e3a11fc87ac51a09b7c001964
wrr606 <wrr606@gmail.com>
2023-05-06 14:23:15 +0800

    re

---
+++ Zerojudge/Uva題庫/c101.cpp
@@ -0,0 +1,18 @@
+//c101. 00122 - Trees on the level
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define pii pair<int,int>
+#define all(x) x.begin(),x.end() 
+#define ll long long
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    string s;
+    while(cin>>s){
+        while(cin>>s&&s!="()"){
+
+        }
+    }
+}
