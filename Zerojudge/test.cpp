d4e426f613b34c513b12769425212b049976a55a
wrr606 <wrr606@gmail.com>
2023-05-21 20:39:23 +0800

    re

--- Zerojudge/test.cpp
+++ Zerojudge/test.cpp
@@ -6,9 +6,30 @@ using namespace std;
 #define all(x) x.begin(),x.end() 
 #define ll long long
 
+#define mann 100
+
+bitset<mann> isprime;
+vector<int> prime;
+
+void primes(){
+    isprime.set(),prime.clear();
+    isprime[0]=0,isprime[1]=0;
+    for(int i=2;i<mann;i++){
+        if(isprime[i])
+            prime.push_back(i);
+        for(auto j:prime){
+            if(i*j>=mann)
+                break;
+            isprime[i*j]=0;
+            if(i%j==0)
+                break;
+        }
+    }
+}
+
 int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
-    string s="123456789";
-    s.erase(0,1);
-    cout<<s;
+    primes();
+    for(int i=0;i<mann;i++){
+        cout<<i<<" "<<isprime[i]<<"\n";
+    }
 }
--- python小工具/test.py
+++ python小工具/test.py
@@ -1,11 +1,8 @@
-arr=list(map(int,input().split()))
-data=[]
-if 0 not in arr:
-    print(arr)
-elif 0 in arr:
-    for i in arr:
-        data.append(i)
-        if i==0:
-            data.append(0)
-        if len(arr)==len(data):
-            print(data)
+from pytube import YouTube
+yt = YouTube('https://www.youtube.com/watch?v=R93ce4FZGbc')   # baby shark 的音樂
+print(yt.title)           # 影片標題
+print(yt.length)          # 影片長度 ( 秒 )
+print(yt.author)          # 影片作者
+print(yt.channel_url)     # 影片作者頻道網址
+print(yt.thumbnail_url)   # 影片縮圖網址
+print(yt.views)           # 影片觀看數
