00d54e860b8c8dbdec83fc77489bb6d35c58eb7a
wrr606 <wrr606@gmail.com>
2023-07-06 15:47:24 +0800

    re

--- Zerojudge/Uva題庫/1451 - Average.cpp
+++ Zerojudge/Uva題庫/1451 - Average.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int sum[100005];
 
 int cmp(int x1,int x2,int x3,int x4){
-    return (sum[x2]-sum[x1])*(x4-x3)-(sum[x4]-sum[x3])*(x2-x1);
+    return (sum[x1]-sum[x2])*(x3-x4)-(sum[x3]-sum[x4])*(x1-x2);
 }
 
 int main(){//math
@@ -20,7 +20,7 @@ int main(){//math
     cin>>t;
     while(t--){
         cin>>n>>l>>s;
-        for(int i=1;i<n;i++)
+        for(int i=1;i<=n;i++)
             sum[i]=sum[i-1]+int(s[i-1])-48;
         int ans_left=0,ans_right=l;
         deque<int> que;
@@ -28,24 +28,24 @@ int main(){//math
             int j=i-l;
             while(que.size()>1){
                 int last1=que.back(),last2=que[que.size()-2];
-                if(cmp(last1,last2,j,last2)>0)
+                if(cmp(last2,last1,last2,j)>0)
                     que.pop_back();
                 else
                     break;
             }
             que.push_back(j);
             while(que.size()>1){
-                if(cmp(i,que[0],i,que[1])<=0)
+                if(cmp(que[0],i,que[1],i)<=0)
                     que.pop_front();
                 else
                     break;
             }
             int cur=cmp(que[0],i,ans_left,ans_right);
-            if(cur>0||cur==0&&ans_left-ans_right>i-que[0]){
+            if(cur>0||cur==0&&ans_right-ans_left>i-que[0]){
                 ans_right=i;
                 ans_left=que[0];
             }
         }
-        cout<<ans_left<<" "<<ans_right<<endl;
+        cout<<1+ans_left<<" "<<ans_right<<endl;
     }
 }
--- Zerojudge/test.cpp
+++ Zerojudge/test.cpp
@@ -7,5 +7,5 @@ using namespace std;
 #define ll long long
 
 int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
+	//ios::sync_with_stdio(0),cin.tie(0);
 }
