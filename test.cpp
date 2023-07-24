8ebe4d7e5dd5bcd2749c409de194591efcca4d8d
wrr606 <wrr606@gmail.com>
2023-07-24 17:54:24 +0800

    re

--- test.cpp
+++ test.cpp
@@ -1,27 +1,16 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
+#define endl '\n'
+#define all(x) x.begin(),x.end() 
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
 int main(){
-  int N,M;
-  cin >> N >> M;
-  list<int> l;
-  for(int i=0;i<N;i++){
-    l.push_back(i);  //放入編號0~N-1
-  }
-  list<int>::iterator it=l.begin();
-  while(N>1){  //直到剩下1人
-    N--;
-    int cnt=1;
-    while(cnt<M){  //找到加一個要刪除的點
-      cnt++;
-      it++;
-      if(it==l.end()) it=l.begin();
-    }
-    auto erase=it;
-    it++;
-    if(it==l.end()) it=l.begin();
-    l.erase(erase);  //刪除
-  } 
-  cout << *it << endl;
-  return 0;
+    //ios::sync_with_stdio(0),cin.tie(0);
 }
