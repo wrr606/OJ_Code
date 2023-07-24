37c3332d8a2ec05c9b3036079f0ceb8ac68207ec
wrr606 <wrr606@gmail.com>
2023-07-24 17:54:41 +0800

    re

--- test.cpp
+++ test.cpp
@@ -1,16 +1,26 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-#define endl '\n'
-#define all(x) x.begin(),x.end() 
-typedef long long int ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<double, double> pdd;
-typedef vector<int> vi;
-typedef vector<ll> vll;
+int main() {
+    std::vector<int> vec = {10, 20, 30, 40, 50};
 
-int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
+    int target = 30;
+    
+    auto lower_it = std::lower_bound(vec.begin(), vec.end(), target);
+    auto upper_it = std::upper_bound(vec.begin(), vec.end(), target);
+
+    if (lower_it != vec.end()) {
+        std::cout << "lower_bound: " << *lower_it << " at index " << std::distance(vec.begin(), lower_it) << std::endl;
+    } else {
+        std::cout << "lower_bound not found" << std::endl;
+    }
+
+    if (upper_it != vec.end()) {
+        std::cout << "upper_bound: " << *upper_it << " at index " << std::distance(vec.begin(), upper_it) << std::endl;
+    } else {
+        std::cout << "upper_bound not found" << std::endl;
+    }
+
+    return 0;
 }
