768767d4929a5b4b521ff9ea5883b4f224e45dcf
wrr606 <wrr606@gmail.com>
2023-07-28 00:31:00 +0800

    re

--- test.cpp
+++ test.cpp
@@ -1,28 +1,51 @@
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <algorithm>
+#include <iostream>
+#include <queue>
 using namespace std;
-using namespace __gnu_pbds;
 
-#define endl '\n'
-#define all(x) x.begin(),x.end() 
-typedef long long int ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<double, double> pdd;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<vector<int> > vvi;
-typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_mutiset;
+const int MAX_N = 2e5;
 
-int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
-    ordered_mutiset om;
-    vi x={1,1,1};
-    for(auto i:x){
-        om.insert(i);
-    }
-    cout<<om.order_of_key(1);
+int N;
+int ans[MAX_N];
+vector<pair<pair<int, int>, int>> v(MAX_N);
+
+int main() {
+	cin >> N;
+	v.resize(N);
+	for (int i = 0; i < N; i++) {
+		cin >> v[i].first.first >> v[i].first.second;
+		v[i].second = i;  // store the original index
+	}
+	sort(v.begin(), v.end());
+
+	int rooms = 0, last_room = 0;
+	priority_queue<pair<int, int>> pq;  // min heap to store departure times.
+	for (int i = 0; i < N; i++) {
+		if (pq.empty()) {
+			last_room++;
+			// make the departure time negative so that we create a min heap
+			// (cleanest way to do a min heap... default is max in c++)
+			pq.push(make_pair(-v[i].first.second, last_room));
+			ans[v[i].second] = last_room;
+		} else {
+			// accessing the minimum departure time
+			pair<int, int> minimum = pq.top();
+			if (-minimum.first < v[i].first.first) {
+				pq.pop();
+				pq.push(make_pair(-v[i].first.second, minimum.second));
+				ans[v[i].second] = minimum.second;
+			}
+
+			else {
+				last_room++;
+				pq.push(make_pair(-v[i].first.second, last_room));
+				ans[v[i].second] = last_room;
+			}
+		}
+
+		rooms = max(rooms, int(pq.size()));
+	}
+
+	cout << rooms << "\n";
+	for (int i = 0; i < N; i++) { cout << ans[i] << " "; }
 }
