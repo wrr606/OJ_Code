2aac985afb784676a0ebe16082ca2a2bcaddbdb8
wrr606 <wrr606@gmail.com>
2023-08-01 17:34:13 +0800

    re

---
+++ CSES/Dynamic Programming/Coin_Combinations_I.cpp
@@ -0,0 +1,35 @@
+//Coin_Combinations_I
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define all(x) x.begin(),x.end() 
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<vector<int> > vvi;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n,x;
+    cin>>n>>x;
+    vi coins(n);
+    for(auto &i:coins)
+        cin>>i;
+    
+}
+/*
+3 9
+2 3 5
+1 2 3 4 5 6 7 8 9
+0 1 1 1 2 2 5 6 8
+*/
