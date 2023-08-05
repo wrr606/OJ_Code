e57b2497dd74a5878335d64e8db97ce58813489f
wrr606 <wrr606@gmail.com>
2023-08-06 00:54:27 +0800

    re

--- CSES/Dynamic Programming/Rectangle_Cutting.cpp
+++ CSES/Dynamic Programming/Rectangle_Cutting.cpp
@@ -1,3 +1,4 @@
+//Rectangle_Cutting
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -20,9 +21,22 @@ typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_upd
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
-    vvi x(3,vi(3,2));
-    for(auto i:x)
-        for(auto j:i)
-            cout<<j<<" ";
+    ios::sync_with_stdio(0),cin.tie(0);
+    int a,b;
+    cin>>a>>b;
+    if(a<b)
+        swap(a,b);
+    vvi dp(a+1,vi(a+1,INT_MAX));
+    for(int i=1;i<=a;i++){
+        dp[i][i]=0;
+    }
+    for(int i=1;i<=a;i++){
+        for(int j=1;j<=b;j++){
+            for(int k=1;k<i;k++)
+                dp[i][j]=min(dp[i][j],dp[i-k][j]+dp[k][j]+1);
+            for(int k=1;k<j;k++)
+                dp[i][j]=min(dp[i][j],dp[i][j-k]+dp[i][k]+1);
+        }
+    }
+    cout<<dp[a][b];
 }
