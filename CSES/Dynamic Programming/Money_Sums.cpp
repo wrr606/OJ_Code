7dada1b08cce51d3ec2123ae3029c071916bf146
wrr606 <wrr606@gmail.com>
2023-08-06 20:53:06 +0800

    re

--- CSES/Dynamic Programming/Money_Sums.cpp
+++ CSES/Dynamic Programming/Money_Sums.cpp
@@ -1,4 +1,4 @@
-//Book_Shop
+//Money_Sums
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -14,23 +14,30 @@ typedef pair<ll, ll> pll;
 typedef pair<double, double> pdd;
 typedef vector<int> vi;
 typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
 typedef vector<vector<int> > vvi;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
+bitset<1000001> dp;
+
 int main(){//01背包
     ios::sync_with_stdio(0),cin.tie(0);
-    int n,x;
-    cin>>n>>x;
-    vll v(n+1),w(n+1),dp(x+1);
-    for(int i=1;i<=n;i++)
-        cin>>w[i];
-    for(int i=1;i<=n;i++)
-        cin>>v[i];
-    for(int i=1;i<=n;i++){
-        for(int j=x;j>=w[i];j--){
-            dp[j]=max(dp[j],dp[j-w[i]]+v[i]);
+    int n,sum=0;
+    cin>>n;
+    vi num(n);
+    for(auto &i:num)
+        cin>>i,sum+=i;
+    dp[0]=1;
+    for(const auto &i:num){
+        for(int j=sum;j>=i;j--){
+            if(!dp[j])
+                dp[j]=dp[j-i];
         }
     }
-    cout<<dp[x];
+    cout<<dp.count()-1<<endl;
+    for(int i=1;i<=sum;i++)
+        if(dp[i])
+            cout<<i<<" ";
 }
