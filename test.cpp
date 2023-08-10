3e9d35bf66d3c142784253c533218e7a6e0fcc1a
wrr606 <wrr606@gmail.com>
2023-08-10 15:23:07 +0800

    re

--- test.cpp
+++ test.cpp
@@ -1,54 +1,54 @@
 #include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 
 #define endl '\n'
-#define all(x) x.begin(),x.end() 
 typedef long long int ll;
-typedef long double ld;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<double, double> pdd;
 typedef vector<int> vi;
 typedef vector<ll> vll;
-typedef vector<string> vs;
-typedef vector<bool> vb;
-typedef vector<vector<int> > vvi;
-typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
-vvi earth(102,vi(102));
+int n;
+vll seg;
 
-bool find_it=0;
+void merge(int idx){
+    seg[idx]=seg[idx<<1]+seg[idx<<1|1];
+}
 
-void BFS(int x,int y){
-    cout<<endl;
-    queue<pii> que;
-    que.push({x,y});
-    while(!que.empty()){
-        pii cur=que.front();
-        que.pop();
-        if(cur.first<=0||cur.first>100||cur.second<=0||cur.second>100||earth[cur.first][cur.second]==1)
-            continue;
-        if(earth[cur.first][cur.second]==-1){
-            cout<<cur.first<<" "<<cur.second<<endl;
-            find_it=1;
-            return;
-        }
-        earth[cur.first][cur.second]=1;
-        que.push({cur.first+1,cur.second});
-        que.push({cur.first,cur.second+1});
-        que.push({cur.first-1,cur.second});
-        que.push({cur.first,cur.second-1});
+void build(int idx=1,int l=1,int r=n){
+    if(l==r){
+        cin>>seg[idx];
+        return;
     }
+    int mid=(l+r)>>1;
+    build(idx<<1,l,mid);
+    build(idx<<1|1,mid+1,r);
+    merge(idx);
+}
+
+ll query(int ql,int qr,int idx,int l,int r){
+    if(ql<=l&&r<=qr)
+        return seg[idx];
+    int mid=(l+r)>>1;
+    if(qr<=mid)
+        return query(ql,qr,idx<<1,l,mid);
+    else if(mid<ql)
+        return query(ql,qr,idx<<1|1,mid+1,r);
+    else
+        return query(ql,qr,idx<<1,l,mid)+query(ql,qr,idx<<1|1,mid+1,r);
 }
 
 int main(){
     ios::sync_with_stdio(0),cin.tie(0);
-    earth[10][20]=-1;
-    BFS(50,50);
-    if(!find_it)
-        cout<<0<<endl;
+    cin>>n;
+    seg.assign((n+5)<<2,0);
+    build();
+    int m;
+    cin>>m;
+    while(m--){
+        int ql,qr;
+        cin>>ql>>qr;
+        if(ql > qr) {
+            swap(ql, qr);
+        }
+        cout<<query(ql,qr,1,1,n)<<endl;
+    }
 }
