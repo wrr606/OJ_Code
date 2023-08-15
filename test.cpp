00a895393a6dad23fb3698ac77f187c2472d77ae
wrr606 <wrr606@gmail.com>
2023-08-15 14:34:54 +0800

    re

--- test.cpp
+++ test.cpp
@@ -1,11 +1,27 @@
 #include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
+#define endl '\n'
+#define all(x) x.begin(),x.end() 
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
 typedef vector<int> vi;
-
-vi x;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 int main(){
-    while(1)
-      x.push_back(1);
+    ll x[100];
+    memset(x,127,sizeof(x));
+    cout<<x[11]<<endl;
+    cout<<LONG_LONG_MAX;
 }
