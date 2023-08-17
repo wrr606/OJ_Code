0e120141685f2ccb25ff0f697077ab553619404e
wrr606 <wrr606@gmail.com>
2023-08-17 23:06:05 +0800

    re

--- test.cpp
+++ test.cpp
@@ -20,8 +20,5 @@ typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_upd
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 int main(){
-    ll x[100];
-    memset(x,127,sizeof(x));
-    cout<<x[11]<<endl;
-    cout<<LONG_LONG_MAX;
+    //ios::sync_with_stdio(0),cin.tie(0);
 }
