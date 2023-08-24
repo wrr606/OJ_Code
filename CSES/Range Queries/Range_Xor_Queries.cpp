e0913056ac404c6d840b6a69d95da07aeb689cb5
wrr606 <wrr606@gmail.com>
2023-08-24 22:24:30 +0800

    re

--- CSES/Range Queries/Range_Xor_Queries.cpp
+++ CSES/Range Queries/Range_Xor_Queries.cpp
@@ -1,3 +1,4 @@
+//Range_Xor_Queries
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -20,7 +21,17 @@ typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_upd
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
 int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
-
-    cout<<(1<<2+2);
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n,q;
+    cin>>n>>q;
+    vll prefix(n+1);
+    for(int i=1;i<=n;i++){
+        cin>>prefix[i];
+        prefix[i]^=prefix[i-1];
+    }
+    while(q--){
+        int a,b;
+        cin>>a>>b;
+        cout<<(prefix[b]^prefix[a-1])<<endl;
+    }
 }
--- CSES/Range Queries/Static_Range_Sum_Queries.cpp
+++ CSES/Range Queries/Static_Range_Sum_Queries.cpp
@@ -1,3 +1,4 @@
+//Static_Range_Sum_Queries
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -19,8 +20,18 @@ typedef vector<vector<int> > vvi;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
-int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
-
-    cout<<(1<<2+2);
+int main(){//prefix
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n,q;
+    cin>>n>>q;
+    vll prefix(n+1);
+    for(int i=1;i<=n;i++){
+        cin>>prefix[i];
+        prefix[i]+=prefix[i-1];
+    }
+    while(q--){
+        int a,b;
+        cin>>a>>b;
+        cout<<prefix[b]-prefix[a-1]<<endl;
+    }
 }
