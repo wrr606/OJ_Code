6cb34b6f6af86568da7519ce33207b7d9cae7812
wrr606 <wrr606@gmail.com>
2023-09-04 22:32:30 +0800

    re

--- Zerojudge/IOI、APCS/i402. 4. 內積.cpp
+++ Zerojudge/IOI、APCS/i402. 4. 內積.cpp
@@ -1,3 +1,4 @@
+//i402. 4. 內積
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -19,6 +20,30 @@ typedef vector<vector<int> > vvi;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
+const int N=1005;
+int A[N],B[N],dp[N][N];
+
 int main(){
-    //ios::sync_with_stdio(0),cin.tie(0);
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++)
+        cin>>A[i];
+    for(int i=0;i<m;i++)
+        cin>>B[i];
+    int ans=INT_MIN;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            dp[i][j]=max(dp[i-1][j-1]+A[i-1]*B[j-1],A[i-1]*B[j-1]);
+            ans=max(ans,dp[i][j]);
+        }
+    }
+    reverse(B,B+m);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            dp[i][j]=max(dp[i-1][j-1]+A[i-1]*B[j-1],A[i-1]*B[j-1]);
+            ans=max(ans,dp[i][j]);
+        }
+    }
+    cout<<ans<<endl;
 }
