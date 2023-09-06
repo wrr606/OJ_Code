437d7cf850e48b98a3d7d1d382fac342f53a65b1
wrr606 <wrr606@gmail.com>
2023-09-06 23:42:12 +0800

    re

---
+++ Zerojudge/競賽題庫/a557. NCPC PB Integer Partition.cpp
@@ -0,0 +1,43 @@
+//a557. NCPC PB Integer Partition
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define all(x) x.begin(),x.end() 
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+ll dp[505][55];
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;//5 2 4
+        memset(dp,0,sizeof(dp));
+        dp[0][0]=1;
+        for(int i=1;i<=c;i++){
+            for(int j=i;j<=a;j++){
+                for(int k=1;k<=b;k++){
+                    dp[j][k]+=dp[j-i][k-1];
+                }
+            }
+        }
+        cout<<dp[a][b]<<endl;
+    }
+}
