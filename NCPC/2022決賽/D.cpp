1cbdf5dcaf175103e9db82e89a6743640a3e5841
wrr606 <wrr606@gmail.com>
2023-09-27 22:08:58 +0800

    re

--- NCPC/2022決賽/D.cpp
+++ NCPC/2022決賽/D.cpp
@@ -19,7 +19,33 @@ typedef vector<vector<int> > vvi;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
+constexpr int N=1e7+5;
+ll y[N]={1},x[N];
+
 int main(){
     ios::sync_with_stdio(0),cin.tie(0);
-    
+    int n,a,b,c,d;
+    while(cin>>n&&n){
+        cin>>a>>b>>c>>d;
+        fill(y+1,y+1+n,0);
+        y[0]=1;
+        for(int i=1;i<=n;i++)
+            y[i]=(y[i-1]*a+b)%c;
+        for(int i=1;i<=n;i++)
+            x[i]=y[i]%d-d/2;
+        ll sum=0,mx=LONG_LONG_MIN,nega=1;
+        pii range={1,1};
+        for(int i=1;i<=n;i++){
+            sum+=x[i];
+            if(sum<0){
+                nega=i;
+                sum=0;
+            }
+            if(sum>mx){
+                mx=sum;
+                range={nega,i};
+            }
+        }
+        cout<<mx<<" "<<range.first<<" "<<range.second<<endl;
+    }
 }
---
+++ Zerojudge/基礎題庫/c716. Johnny B. Goode.py
@@ -0,0 +1,1 @@
+print(f"Go, {input()}, go go")
