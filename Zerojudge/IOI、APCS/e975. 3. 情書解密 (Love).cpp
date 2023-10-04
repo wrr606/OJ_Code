7111b802d6e6ce16f51ca7c409a77e7d46fb5c02
wrr606 <wrr606@gmail.com>
2023-10-04 17:27:00 +0800

    re

--- Zerojudge/IOI、APCS/e975. 3. 情書解密 (Love).cpp
+++ Zerojudge/IOI、APCS/e975. 3. 情書解密 (Love).cpp
@@ -1,3 +1,4 @@
+//e975. 3. 情書解密 (Love)
 #include<bits/stdc++.h>
 #include<ext/pb_ds/assoc_container.hpp>
 #include<ext/pb_ds/tree_policy.hpp>
@@ -18,6 +19,7 @@ typedef vector<bool> vb;
 typedef vector<vector<int> > vvi;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
 typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
 char low(int c){
     if(c<97)
         return 122-96+c;
@@ -33,9 +35,25 @@ char high(int c){
         return c-90+64;
     return c;
 }
+
 int main(){
-	ios::sync_with_stdio(0),cin.tie(0);
-	char a='Z'-1;
-	cout<<low(a)<<endl;
-	cout<<high(a)<<endl;
+    ios::sync_with_stdio(0),cin.tie(0);
+    string s,love="love",Love="Love";
+    getline(cin,s);
+    if(s.find(love)!=s.npos||s.find(Love)!=s.npos){
+        cout<<0;
+        return 0;
+    }
+    for(int i=1;i<26;i++){
+        for(auto &j:love)
+            j=low(--j);
+        Love[0]=high(--Love[0]);
+        for(int j=1;j<4;j++)
+            Love[j]=low(--Love[j]);
+        
+        if(s.find(love)!=s.npos||s.find(Love)!=s.npos){
+            cout<<i;
+            return 0;
+        }
+    }
 }
--- python小工具/test.py
+++ python小工具/test.py
@@ -1,3 +1,3 @@
-class node:
-    def __init__(self) -> None:
-        pass
+If = lambda c:lambda x:lambda y:c(x)(y)
+x=10
+If(x==10,print(123),print(0))
