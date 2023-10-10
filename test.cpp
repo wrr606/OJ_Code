cee3fed6711afc07d83c987563f5c8f2299777f7
wrr606 <wrr606@gmail.com>
2023-10-10 14:36:24 +0800

    re

--- test.cpp
+++ test.cpp
@@ -1,11 +1,11 @@
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
 using namespace __gnu_pbds;
 
 #define endl '\n'
-#define all(x) x.begin(),x.end() 
+#define all(x) x.begin(), x.end()
 typedef long long int ll;
 typedef long double ld;
 typedef pair<int, int> pii;
@@ -15,11 +15,20 @@ typedef vector<int> vi;
 typedef vector<ll> vll;
 typedef vector<string> vs;
 typedef vector<bool> vb;
-typedef vector<vector<int> > vvi;
-typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
-typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+typedef vector<vector<int>> vvi;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
 
-int main(){
-    ios::sync_with_stdio(0),cin.tie(0);
-    
+int main()
+{
+    vi nums={41,33,29,33,35,26,47,24,18,28};
+    sort(all(nums));
+    for(auto i:nums)
+        cout<<i<<" ";
+        cout<<endl;
+    const int length = nums.size() - 1;
+    for(const auto &i:nums){
+        auto mn=lower_bound(all(nums),i-length),mx=upper_bound(all(nums),i-length);
+        
+    }
 }
