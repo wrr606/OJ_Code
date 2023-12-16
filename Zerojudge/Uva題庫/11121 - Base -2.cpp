2394352e329bb1a73104e2977bd0163d70cb01c5
wrr606 <wrr606@gmail.com>
2023-12-16 13:43:22 +0800

    2023/12/16

---
+++ Zerojudge/Uva題庫/11121 - Base -2.cpp
@@ -0,0 +1,47 @@
+//11121 - Base -2
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define all(x) x.begin(),x.end() 
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    int t;
+    cin>>t;
+    for(int c=1;c<=t;c++){
+        int n;
+        cin>>n;
+        cout<<"Case #"<<c<<": ";
+        if(!n){
+            cout<<0<<endl;
+            continue;
+        }
+        vi ans;
+        while(n){
+            ans.push_back(n%(-2));
+            n=n/(-2);
+            if(ans.back()<0)
+                ans.back()=1,n++;
+        }
+        reverse(all(ans));
+        for(const auto &i:ans)
+            cout<<i;
+        cout<<endl;
+    }
+}
