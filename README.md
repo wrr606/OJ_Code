46f4bb7a49c9c65600f70bc7d71b6cea05e93754
wrr606 <wrr606@gmail.com>
2024-01-04 21:42:44 +0800

    2024/1/4

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Online Judge Code
+---
+這邊是我放Code的地方，可能會放些Online Judge的Code或是自己做的一些腳本小工具
+
+主要以C++為主，有部分題目會使用Python
--- python小工具/test case maker.py
+++ python小工具/test case maker.py
@@ -1,4 +1,9 @@
 import os
+import random
+
+class Solution:
+    def minPairSum(self, nums) -> int:
+        return nums.sort() or max(nums[i]+nums[-1-i] for i in range((len(nums)>>1)+1))
 
 def save_keys_to_text_files(dictionary, output_folder):
     os.makedirs(output_folder, exist_ok=True)
@@ -12,8 +17,42 @@ def save_keys_to_text_files(dictionary, output_folder):
             file.write(str(dictionary[key]))
         cnt+=1
 
-keys_data = ["12 3", "1200 5", "1200 1200", "3 2", "25326 3652", "456987 326596", "8492 142", "15881 300", "10677 119", "1000 5"]
-values_data = [17, 1499, 1201, 5, 25332, 456988, 8552, 15934, 10767, 1249]
+s=Solution()
+keys_data = []
+values_data = []
+
+for j in range(0,3):
+    temp=[]
+    random_number = random.randint(2, 50) #n
+    keys_data.append(f"{random_number}\n")
+    for i in range(random_number):
+        temp.append(random.randint(1, 10**2))
+        random.shuffle(temp)
+    x=temp.copy()
+    values_data.append(s.minPairSum(x))
+    keys_data[-1]+=" ".join(map(str,temp))
+
+for j in range(3,6):
+    temp=[]
+    random_number = random.randint(100, 500) #n
+    keys_data.append(f"{random_number}\n")
+    for i in range(random_number):
+        temp.append(random.randint(1, 10**5))
+        random.shuffle(temp)
+    x=temp.copy()
+    values_data.append(s.minPairSum(x))
+    keys_data[-1]+=" ".join(map(str,temp))
+
+for j in range(6,10):
+    temp=[]
+    random_number = random.randint(500, 1000) #n
+    keys_data.append(f"{random_number}\n")
+    for i in range(random_number):
+        temp.append(random.randint(1, 10**8))
+        random.shuffle(temp)
+    x=temp.copy()
+    values_data.append(s.minPairSum(x))
+    keys_data[-1]+=" ".join(map(str,temp))
 
 data_dict = dict(zip(keys_data, values_data))
 
