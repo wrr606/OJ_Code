64b1750b2dae6b72c3d7d7bcb6a6c4d920eaf395
wrr606 <wrr606@gmail.com>
2025-10-17 23:15:29 +0800

    2025/10/17

---
+++ NTHU_hw/W5/W5-1.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define endl '\n'
+#define all(x) x.begin(),x.end() 
+typedef long long int ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<vector<int> > vvi;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+priority_queue<int> mx;
+priority_queue<int, vi, greater<>> mn;
+
+int get_mid(){
+    return mx.top();
+}
+
+void adjust(){
+    mn.emplace(mx.top()), mx.pop();
+    if(mx.size() < mn.size())
+        mx.emplace(mn.top()), mn.pop();
+}
+
+int main(){
+    ios::sync_with_stdio(0),cin.tie(0);
+    int n, k;
+    cin>>n;
+    while(n--){
+        cin>>k;
+        mx.emplace(k);
+        adjust();
+        cout<<get_mid()<<" ";
+    }
+}
